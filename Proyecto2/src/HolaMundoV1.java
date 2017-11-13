0061a1930e8227db9c122df8ddee77b2c5d49774
tubidj <martindperez@gmail.com>
2017-11-13 13:15:23 -0200

    version definitiva

--- Proyecto2/src/HolaMundoV1.java
+++ Proyecto2/src/HolaMundoV1.java
@@ -3,12 +3,9 @@ public class HolaMundoV1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hola Mundo v2");
-System.out.println("Hola Mundo V3");
-System.out.println("Hola Mundo V3");
+System.out.println("Hola Mundo Version definitiva");
 
-System.out.println("Hola Mundo prueba3");
-System.out.println("nueva modificacion3");
+
 	}
 
 }
